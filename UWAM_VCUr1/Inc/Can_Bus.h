4a1a9ae34e65f6fea919dd7cb03477bd5ad4d2b5
Tom <thomas.paynter@student.curtin.edu.au>
2018-11-11 20:00:00 +0800

    Routine

---
+++ UWAM_VCUr1/Inc/Can_Bus.h
@@ -0,0 +1,195 @@
+// Inverter Settings
+#define MAX_TORQUE 140
+
+// CAN Receive IDs
+#define PEDALBOX_ID 0x3C
+
+// CAN Transmit IDs
+#define INVERTER_ID 0x0C0
+#define PDM_ID 0x28C
+
+// ****** End User Settings ************************************
+
+#include <string>
+
+extern "C" {
+#include "stm32f0xx_hal.h"
+#include "can.h"
+#include "gpio.h"
+}
+
+class Can_Bus {
+public:
+	static Can_Bus* Instance() {
+//		Can Bus Singleton
+		if (!m_pInstance)
+			m_pInstance = new Can_Bus;
+
+		return m_pInstance;
+	}
+
+private:
+	CanRxMsgTypeDef RxMessage;
+	CanTxMsgTypeDef TxMessage;
+
+	uint8_t torque_request;
+	uint8_t brake_pressure;
+	uint8_t implausibility;
+	Can_Bus* m_pInstance = NULL;
+	uint8_t cooling_state;
+	uint16_t current_torque;
+
+	uint8_t recievedpedalbox = 0;
+
+	Can_Bus(void) {
+		torque_request = 0;
+		brake_pressure = 0;
+		implausibility = 0;
+		cooling_state = 0;
+		current_torque = 0;
+	}
+
+	void init(void) {
+		TxMessage.IDE = CAN_ID_STD;
+		TxMessage.RTR = CAN_RTR_DATA;
+		hcan.pTxMsg = &TxMessage;
+		hcan.pRxMsg = &RxMessage;
+
+		CAN_FilterConfTypeDef sFilterConfig;
+
+		sFilterConfig.FilterNumber = 0;
+		sFilterConfig.FilterMode = CAN_FILTERMODE_IDLIST;
+		sFilterConfig.FilterScale = CAN_FILTERSCALE_16BIT;
+		sFilterConfig.FilterIdHigh = PEDALBOX_ID;
+		sFilterConfig.FilterIdLow = PEDALBOX_ID << 5;
+
+		sFilterConfig.FilterFIFOAssignment = CAN_FIFO0;
+		sFilterConfig.FilterActivation = ENABLE;
+		HAL_CAN_ConfigFilter(&hcan, &sFilterConfig);
+
+//		sFilterConfig.FilterNumber = 1;
+//		sFilterConfig.FilterIdHigh = CANID_2;
+//		sFilterConfig.FilterIdLow = CANID_2 << 5;
+//		HAL_CAN_ConfigFilter(&hcan, &sFilterConfig);
+
+		HAL_CAN_Receive_IT(&hcan, CAN_FIFO0);
+
+	}
+
+	void rx_update(void) {
+		switch (RxMessage.StdId) {
+		case PEDALBOX_ID:
+			torque_request = RxMessage.Data[0];
+			brake_pressure = RxMessage.Data[2];
+			implausibility = RxMessage.Data[3];
+
+			recievedpedalbox = 1;
+			break;
+
+		}
+		HAL_CAN_Receive_IT(&hcan, CAN_FIFO0);
+	}
+
+	uint8_t recievedPedalBox() {
+		return recievedpedalbox;
+	}
+
+	void clearPedalBox() {
+		recievedPedalBox = 0;
+	}
+
+	void cooling_on() {
+		TxMessage.StdId = PDM_ID;
+		TxMessage.DLC = 1;
+		TxMessage.Data[0] = 0b10000000;
+		cooling_state = 1;
+		HAL_CAN_Transmit_IT(&hcan);
+
+	}
+
+	void cooling_off() {
+		TxMessage.StdId = PDM_ID;
+		TxMessage.DLC = 1;
+		TxMessage.Data[0] = 0b00000000;
+		cooling_state = 0;
+		HAL_CAN_Transmit_IT(&hcan);
+
+	}
+
+	uint8_t get_cooling_state() {
+		return cooling_state;
+	}
+
+	void set_torque(uint16_t torque_command) {
+		TxMessage.StdId = INVERTER_ID;
+		TxMessage.DLC = 8;
+
+		if (torque_command > MAX_TORQUE)
+			torque_command = MAX_TORQUE;
+
+//		Inverter Torque Settings
+		TxMessage.Data[0] = torque_command % 256;
+		TxMessage.Data[1] = torque_command/256;
+
+//		Inverter Speed Settings (Unused Set 0)
+		TxMessage.Data[2] = 0;
+		TxMessage.Data[3] = 0;
+
+//		Motor Direction - Forward
+		TxMessage.Data[4] = 1;
+
+//		Inverter Run - Run
+		TxMessage.Data[5] = 1;
+
+//		Unknown set 0
+		TxMessage.Data[6] = 0;
+		TxMessage.Data[7] = 0;
+
+		HAL_CAN_Transmit_IT(&hcan);
+
+	}
+
+	uint16_t get_current_torque() {
+		return current_torque;
+	}
+
+	void arm_inverter() {
+		TxMessage.StdId = INVERTER_ID;
+		TxMessage.DLC = 8;
+		TxMessage.Data[0] = 0;
+		TxMessage.Data[1] = 0;
+		TxMessage.Data[2] = 0;
+		TxMessage.Data[3] = 0;
+		TxMessage.Data[4] = 0;
+		TxMessage.Data[5] = 0;
+		TxMessage.Data[6] = 0;
+		TxMessage.Data[7] = 0;
+
+		HAL_CAN_Transmit_IT(&hcan);
+
+	}
+
+	void rx_print() {
+		printf("Receiving CAN:  ID: %d DLC: %d Data: ", RxMessage.StdId,
+				RxMessage.DLC);
+
+		for (int i = 0; i < RxMessage.DLC; i++)
+			printf("%d ", RxMessage.Data[i]);
+
+		printf("\n\r");
+
+	}
+
+	void tx_print() {
+
+		printf("\tTransmitting CAN:  ID: %d DLC: %d Data: ", TxMessage.StdId,
+				TxMessage.DLC);
+
+		for (int i = 0; i < TxMessage.DLC; i++)
+			printf("%d ", TxMessage.Data[i]);
+
+		printf("\n\r");
+	}
+
+}
+;
---
+++ UWAM_VCUr1/Src/main.cpp
@@ -0,0 +1,79 @@
+extern "C" {
+#include "main.h"
+#include "stm32f0xx_hal.h"
+#include "can.h"
+#include "gpio.h"
+#include "Can_Bus.h"
+}
+
+void SystemClock_Config(void);
+
+extern "C" {
+#include <stdio.h>
+
+int __io_putchar(int ch) {
+
+//	uint8_t character = (uint8_t) ch;
+//	HAL_UART_Transmit(&huart2, &character, 1, 1000);
+//	return ch;
+}
+}
+
+int main(void) {
+
+	HAL_Init();
+
+	SystemClock_Config();
+
+	MX_GPIO_Init();
+	MX_CAN_Init();
+
+	Can_Bus can_bus = can_bus.Instance();
+
+	while (1) {
+
+	}
+
+}
+
+void SystemClock_Config(void) {
+
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+	RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		_Error_Handler(__FILE__, __LINE__);
+	}
+
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+		_Error_Handler(__FILE__, __LINE__);
+	}
+
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
+
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+void _Error_Handler(char *file, int line) {
+	while (1) {
+	}
+}
+
+#ifdef  USE_FULL_ASSERT
+
+void assert_failed(uint8_t* file, uint32_t line)
+{
+
+}
+#endif
